7e4788ed22453465c2ee57e11ff59efede008ef1
Zwe Let Yar <letyarzwe@gmail.com>
2026-06-29 08:28:28 +0630

    fix image

--- app/(root)/design/page.tsx
+++ app/(root)/design/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
         {/* Image Section */}
         <div className="overflow-hidden rounded-2xl bg-black/20">
           <Image
-            src="/jacket4.JPG"
+            src="/jacket4.jpg"
             alt="jacket design"
             width={500}
             height={700}
